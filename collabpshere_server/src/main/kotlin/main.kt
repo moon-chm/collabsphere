66098cf7f1873736f82533c78b26268ad5f32afc
moon-chm <your-github-email@example.com>
2026-09-08 11:58:57 +0530

    feat(server): add HEAD and GET handlers for root and health routes to support UptimeRobot

--- collabpshere_server/src/main/kotlin/main.kt
+++ collabpshere_server/src/main/kotlin/main.kt
@@ -23,6 +23,7 @@ fun Application.module() {
     install(CORS) {
         anyHost()
         allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Head)
         allowMethod(HttpMethod.Post)
         allowMethod(HttpMethod.Get)
         allowMethod(HttpMethod.Put)
